e984df5bf005d4bcccf82c3c6e5fd1a0f5300d0a
Edelgard <jtboyman@gmail.com>
2021-11-14 12:11:48 -0500

    build html page

--- app.js
+++ app.js
@@ -1,7 +1,33 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log ('Portfolio complete! Check out index.html to see the output!');
+});
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* helpful for loop info
 const printProfileData = profileDataArr => {
     //for loop
     for (let i=0; i<profileDataArr.length; i++) {
@@ -13,7 +39,4 @@ const printProfileData = profileDataArr => {
     profileDataArr.forEach(profileItem => console.log(profileItem));
 
 };
-
-
-
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs);*/
